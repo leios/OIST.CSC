847cdb51e60c4518dd4b326e60c4bfae85a52e83
James Schloss <james.schloss@oist.jp>
2014-09-14 12:36:58 -0500

    added the first lesson for python. It's pretty short, sweet and to the point.

---
+++ python/Lesson_1/hello_world.py
@@ -0,0 +1,22 @@
+#-------------hello_world.py---------------------------------------------------#
+#
+# In most traditional coding courses, they start with a simple program that 
+# outputs "Hello World!" to the terminal. Luckily, in python... this is pretty
+# simple. In fact, it's only one line (the line that follows this long comment).#
+# You should be able to type in:
+#
+# python hello_world.py
+#
+# and receive the following output in your terminal:
+#
+# Hello World!
+#
+# So let's get to it!
+#------------------------------------------------------------------------------#
+
+print("Hello World!")
+
+# In python, the "print" command sends whatever is in the parentheses to your 
+# terminal. That's precisely what we are doing. Sending "Hello World!" to the
+# terminal. Let me know if you have any trouble!
+
